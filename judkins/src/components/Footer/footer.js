7a93a121182dce07ba83e6ad2943d7cb1990463f
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-12-09 12:38:01 -0800

    getting basics rendering

---
+++ judkins/src/components/Footer/footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  copyright: {
+    marginTop: 10
+  }
+
+}))
+
+function Footer() {
+  const classes = useStyles();
+  return (
+    <>
+      <Typography variant="body2"
+        color="textSecondary"
+        align="center" 
+        className={classes.copyright}> Designed by <a href="" target="blank">Jake Levin</a></Typography>
+      <Typography
+        className={classes.copyright}
+        variant="body2"
+        color="textSecondary"
+        align="center"
+      >
+        {"Copyright © "}
+        <Link color="inherit" href='mailto:christopherhamersly@gmail.com'>
+          Chris Hamersly
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </>
+  )
+}
+
+export default Footer
