18964700e162a1088384966e8b7df2791faeba76
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2021-01-07 15:21:10 -0800

    rules updated

--- judkins/src/components/Rules/rules.js
+++ judkins/src/components/Rules/rules.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
 
 function Rules() {
   return (
-    <h1> Rules </h1>
+    <>
+      <img
+        align='center'
+        src='https://i.ibb.co/fQTV6Fb/Juddy.jpg'
+        alt='judkins'
+        height='650'
+        width='650' />
+      <h1> Rules </h1>
+      <Typography variant="h4">
+        Here are the list of rules:
+    </Typography>
+    
+    <Typography variant="h5">
+        Don't be a dick
+    </Typography>
+    <Typography variant="h5">
+        Share the space
+    </Typography>
+    <Typography variant="h5">
+        Use your words
+    </Typography>
+    <Typography variant="h5">
+        Ask always ask.
+    </Typography>
+    <Typography variant="h5">
+        Be kind
+    </Typography>
+      <Typography>
+        <a href="" target="blank">Blank</a>
+      </Typography>
+      <Typography>
+        <a href="" target="blank">Blank</a>
+      </Typography>
+      <Typography>
+        <a href="" target="blank">Blank</a>
+      </Typography>
+      <Typography>
+        <a href="" target="blank">Blank</a>
+      </Typography>
+    </>
   )
 }
 
